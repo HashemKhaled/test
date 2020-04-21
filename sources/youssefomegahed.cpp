5dc4c74bf5c329c8f2493e6a62e9081237520540
youssefomegahed <youssefosama@aucegypt.edu>
2020-04-21 16:46:53 +0200

    message

---
+++ sources/youssefomegahed.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "youssefomegahed.h"
+using namespace std;
+
+void youssefomegahed
+{
+	cout<<"username: youssefomegahed"<<endl;
+	cout<<"email: youssefosama@aucegypt.edu";
+}
